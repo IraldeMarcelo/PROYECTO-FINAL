979ef0dc913cc0bb200a083569cc09a234f2d94d
Iralde <marceloiralde1996esnm@gmail.com>
2023-01-20 18:18:33 -0300

    Proyecto Final

---
+++ js/cv.js
@@ -0,0 +1,12 @@
+
+
+document.getElementById("btn").addEventListener("click", function () {
+    document.body.style.backgroundColor = "grey";
+});
+
+function saludar(){
+	console.log("Bienvenido estimado Usuario");
+	alert("Bienvenido estimado Usuario")
+}
+
+saludar("Bienvenido estimado Usuario")
